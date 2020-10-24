226bd6ce276329e9784371bb934987ae2427dcfb
sava911 <pesss929@gmail.com>
2020-10-24 14:20:24 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './App.css';
+import Car from "./Car/Car";
+class App extends Component {
+
+    render() {
+        const divStyle = {
+            textAlign: 'center'
+        }
+
+        return (
+            <div style={divStyle} >
+                <h1>Hello Savchenk</h1>
+                <Car/>
+            </div>
+        );
+    }
+
+
+}
+
+export default App;
---
+++ src/Car/Car.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default () => (
+    <div>
+        This is car component
+        <strong>test</strong>
+    </div>
+)
+
+
+
