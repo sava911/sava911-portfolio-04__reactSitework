abecc3ff67668785844b84bf463834081bc97d8b
sava911 <pesss929@gmail.com>
2020-12-08 23:29:09 +0300

    dime clasess

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -2,15 +2,31 @@ import React from "react";
 import './Car.css'
 
 
-export default (props) => (
-    <div className="Car">
+export default (props) => {
+    const inputClasses = ['input']
+    if(props.name !==''){
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+    if(props.name.length > 4) {
+        inputClasses.push('bold')
+    }
 
-        <h3>Car name: {props.name}</h3>
-        <p>Year: <strong>{props.year}</strong></p>
-        <input type="text" onChange={props.onChangeName} value={props.name}/>
-        <button onClick={props.onDelete}>Delete</button>
-    </div>
-)
+    return (
+        <div className="Car">
+
+            <h3>Car name: {props.name}</h3>
+            <p>Year: <strong>{props.year}</strong></p>
+            <input
+                type="text"
+                onChange={props.onChangeName}
+                className={inputClasses.join(' ')}
+                value={props.name}/>
+            <button onClick={props.onDelete}>Delete</button>
+        </div>
+    )
+}
 
 
 
